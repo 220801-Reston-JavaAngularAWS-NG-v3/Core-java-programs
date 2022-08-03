7ae0ccf9a8cb87ed8eff186e275b9e4cd0f959d3
Poonga <bpoongs1982@gmail.com>
2022-08-03 13:05:09 -0400

    Initial commit

---
+++ FirstProject/src/FlowControlDemo.java
@@ -0,0 +1,24 @@
+
+public class FlowControlDemo {
+
+	public static void main(String[] args) {
+		// Flow control statements
+		// 1) if
+		//		a) simple if
+		//		b) if...else
+		//      c) else if ladder
+		//      d) nested if
+		// 2) switch...case
+		
+		// Looping statement
+		// 1) while loop
+		// 2) do...while loop
+		// 3) for loop
+		
+		// break
+		// continue
+
+		
+	}
+
+}

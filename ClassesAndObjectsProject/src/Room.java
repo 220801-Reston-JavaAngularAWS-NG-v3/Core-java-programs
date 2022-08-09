dd2bed7efa036de6d2785abf4e98dfe8ef6d2366
Poonga <bpoongs1982@gmail.com>
2022-08-09 17:39:23 -0400

    OOPS Concept

--- ClassesAndObjectsProject/src/Room.java
+++ ClassesAndObjectsProject/src/Room.java
@@ -1,39 +1,47 @@
 
 public class Room {
-	int length;
-	int width;
-	int height;
-	String color;
-	int roomNo;
-	int floorArea;
-	int wallArea;
-	
-	
-	Room(int length, int width, int height){
-		//super(); // if you have note provided this() or super()
+
+	// declare variables
+	// static blocks
+	// constructors
+	// getters/setters
+	// methods
+	// if required equals and hashcode methods
+	// toString() method
+
+	private int length;
+	private int width;
+	private int height;
+	private String color;
+	private int roomNo;
+	private int floorArea;
+	private int wallArea;
+
+	Room(int length, int width, int height) {
+		// super(); // if you have note provided this() or super()
 		this.length = length;
 		this.width = width;
 		this.height = height;
-		
+
 	}
-	
-	Room(int length, int width, int height, String color){
+
+	Room(int length, int width, int height, String color) {
 		this(length, width, height); // constructor chaining
 //		this.length = length;
 //		this.width = width;
 //		this.height = height;
 		this.color = color;
 	}
-	
-	Room(int length, int width, int height, int roomNo ){
+
+	Room(int length, int width, int height, int roomNo) {
 		this(length, width, height); // constructor chaining
 //		this.length = length;
 //		this.width = width;
 //		this.height = height;
 		this.roomNo = roomNo;
 	}
-	
-	Room(int length, int width, int height, String color, int roomNo){
+
+	Room(int length, int width, int height, String color, int roomNo) {
 		this(length, width, height, color); // constructor chaining
 //		this.length = length;
 //		this.width = width;
@@ -41,15 +49,106 @@ public class Room {
 //		this.color = color;
 		this.roomNo = roomNo;
 	}
-	
+
+	// getter for length property
+	public int getLength() {
+		return length;
+	}
+
+	// setter for length property
+//	public void setLength(int length) {
+//		this.length = length;
+//	}
+
+	// getter for color property
+	public String getColor() {
+		return color;
+	}
+
+	// setter for color property
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+//	public void setWidth(int width) {
+//		this.width = width;
+//	}
+
+	public int getHeight() {
+		return height;
+	}
+
+//	public void setHeight(int height) {
+//		this.height = height;
+//	}
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+
+	public int getFloorArea() {
+		return floorArea;
+	}
+
+	public void setFloorArea(int floorArea) {
+		this.floorArea = floorArea;
+	}
+
+	public int getWallArea() {
+		return wallArea;
+	}
+
+	public void setWallArea(int wallArea) {
+		this.wallArea = wallArea;
+	}
+
 	int calculateFloorArea() {
 		floorArea = length * width;
 		return floorArea;
 	}
-	
+
 	int calculatePaintingCost(int paintingRate) {
-		wallArea = (2 * length * height) + ( 2* width * height);
+		wallArea = (2 * length * height) + (2 * width * height);
 		int paintingCost = wallArea * paintingRate;
 		return paintingCost;
-	}	
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		// provide my definition of room objects equality
+		// the current objects length, width and height should be the same as the other object length, width and height
+		Room otherRoom = (Room)other;
+		if(this.length == otherRoom.length && this.width == otherRoom.width && this.height == otherRoom.height) {
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+		
+	// whenever you overirde the equals methods you should override the hashcode method() as well
+	@Override
+	public int hashCode() {
+		return length + width + height;
+	}
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Room [length=" + length + ", width=" + width + ", height=" + height + "]";
+	}
+	
+	
+	
 }
--- ClassesAndObjectsProject/src/RoomDemo.java
+++ ClassesAndObjectsProject/src/RoomDemo.java
@@ -19,6 +19,8 @@ public class RoomDemo {
 //		bedroom.length = 200;
 //		bedroom.width = 100;
 //		bedroom.height = 150;
+		//bedroom.color = "Pink"; // not possible because we have made the data members private
+		bedroom.setColor("Pink"); // going forward we have to use the setter method to set the values for the private properties
 		
 		System.out.println("bedroom's floor area:" + bedroom.calculateFloorArea());
 		System.out.println("bedrooms' painting cost:" + bedroom.calculatePaintingCost(2));
@@ -32,10 +34,40 @@ public class RoomDemo {
 		System.out.println("dinning's floor area:" + dinning.calculateFloorArea());
 		System.out.println("dinning's painting cost:" + dinning.calculatePaintingCost(2));
 		
+		System.out.println("-----------------------");
 		
 		Room living = new Room(400, 300, 150, "cream", 501);
 
+		System.out.println(living.getLength());
+		System.out.println(living.getWidth());
+		System.out.println(living.getHeight());
+		System.out.println(living.calculateFloorArea());
+		
+		System.out.println(living); // will print the address,
+									//if you want something meaningful to be printed over the toString() method
+									// this implicitly calls the toString() 
+		System.out.println(living.toString()); // line 46 also implcitly calls the toString() method
+												// our Room class does not have the toString() method
+												// to toString() method comes from the Object super class
 		// CPD - Copy Paste Detector
+		
+		System.out.println("----------------------------");
+		Room room1 = new Room(200, 100, 150);
+		Room room2 = new Room(200, 100, 150);
+		Room room3 = new Room(600, 400, 150);
+		Room room4 = room3;
+		
+		System.out.println("room1==room2 : " + (room1 == room2)); 
+		System.out.println("room2==room3 : " + (room2 == room3));
+		System.out.println("room3==room1 : " + (room3 == room1));
+		System.out.println("room4==room3 : " + (room4 == room3));
+		
+		System.out.println("----------------------------");
+		
+		System.out.println("room1.equals(room2) : " + (room1.equals(room2))); 
+		System.out.println("room2.equals(room3) : " + (room2.equals(room3)));
+		System.out.println("room3.equals(room1) : " + (room3.equals(room1)));
+		System.out.println("room4.equals(room3) : " + (room4.equals(room3)));
 	}
 
 }

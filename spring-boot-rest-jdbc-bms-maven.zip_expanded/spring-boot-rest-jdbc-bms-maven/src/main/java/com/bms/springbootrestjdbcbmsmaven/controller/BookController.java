f2e990c9d7f706706aeca2e153438c60fad96e68
Poonga <bpoongs1982@gmail.com>
2022-09-14 11:05:29 -0400

    rest methods completed

--- spring-boot-rest-jdbc-bms-maven.zip_expanded/spring-boot-rest-jdbc-bms-maven/src/main/java/com/bms/springbootrestjdbcbmsmaven/controller/BookController.java
+++ spring-boot-rest-jdbc-bms-maven.zip_expanded/spring-boot-rest-jdbc-bms-maven/src/main/java/com/bms/springbootrestjdbcbmsmaven/controller/BookController.java
@@ -3,7 +3,12 @@ package com.bms.springbootrestjdbcbmsmaven.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +20,10 @@ import com.bms.springbootrestjdbcbmsmaven.service.BookService;
 @RequestMapping("api/books")
 public class BookController {
 
-	@Autowired
+	// by autowiring we are telling the spring framework that search for a class that implements this interface(BookService)
+		// and create an object of that class and assign it to this reference variable bookService
+		// provided the class implementation is annotated with @Component or one of its stereotype annotation(@Service)
+	@Autowired 
 	BookService bookService;
 	
 	// write the 5 rest methods corresponding to the 5 endpoints
@@ -32,13 +40,32 @@ public class BookController {
 
 	}
 	// Read - getABook - @GetMapping
+	// http://localhost:6666/api/books/14
+	// here we have 2 types of parmaters that can be sent through the url
+		// query param
+		// request param
+	@GetMapping("/{bid}")
+	public BookPojo getABook(@PathVariable("bid") int  bookId) throws ApplicationException {
+		return bookService.getABook(bookId);
+	}
 	
 	// Create - addBook - @PostMapping
+	@PostMapping("")
+	public BookPojo addBook(@RequestBody BookPojo newBook) throws ApplicationException {
+		return bookService.addBook(newBook);
+	}
 	
 	// Update - updateBook - @PutMapping
+	@PutMapping("")
+	public BookPojo updateBook(@RequestBody BookPojo updateBook) throws ApplicationException {
+		return bookService.updateBook(updateBook);
+	}
 	
 	// Delete - deleteBook - @DeleteMapping
-	
+	@DeleteMapping("/{bid}")
+	public void deleteBook(@PathVariable("bid") int bookId) throws ApplicationException {
+		bookService.deleteBook(bookId);
+	}
 	
 	
 }
